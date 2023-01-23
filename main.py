c4069aad9f8d565ace2604b0e3654afd38e8b7de
splicid <luis1abreu11@gmail.com>
2023-01-22 22:08:52 -0500

    Connection and some GUI changes

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
 from tkinter import *
 from tkinter import ttk
-from pymongo import MongoClient
+from pymongo_get_database import get_database
+
+dbname = get_database()
+collection_name = dbname["items"]
+
+window = Tk()
+window.title(" Database builder ")
+window.geometry('1000x400')
+window.configure(background = "white");
+
+fist_var = StringVar()
+last_var = StringVar()
+email_var = StringVar()
+contact_var = IntVar()
 
 
-
-def get_database():
- 
-   # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = ""
- 
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
-   # Create the database for our example (we will use the same database throughout the tutorial
-   return client['GUI_App']
-  
-# This is added so that many files can reuse the function get_database()
-if __name__ == "__main__":
-   # Get the database
-   dbname = get_database()
+a = Label(window ,text = "First Name").grid(row = 0,column = 0)
+b = Label(window ,text = "Last Name").grid(row = 1,column = 0)
+c = Label(window ,text = "Email").grid(row = 2,column = 0)
+d = Label(window ,text = "Contact Number").grid(row = 3,column = 0)
+a1 = Entry(window, textvariable= fist_var).grid(row = 0,column = 1)
+b1 = Entry(window, textvariable= last_var).grid(row = 1,column = 1)
+c1 = Entry(window, textvariable= email_var).grid(row = 2,column = 1)
+d1 = Entry(window, textvariable= contact_var).grid(row = 3,column = 1)
 
 
-# window = Tk()
-# window.title(" Database builder ")
-# window.geometry('400x400')
-# window.configure(background = "white");
+column_name = ("Fullname", "Email Address", "Phone Number")
+treeviews = ttk.Treeview(window)
+treeviews.configure(columns=column_name)
 
-# fist_var = StringVar()
-# last_var = StringVar()
-# email_var = StringVar()
-# contact_var = IntVar()
+treeviews.grid(row=5, column=1)
 
 
-# a = Label(window ,text = "First Name").grid(row = 0,column = 0)
-# b = Label(window ,text = "Last Name").grid(row = 1,column = 0)
-# c = Label(window ,text = "Email").grid(row = 2,column = 0)
-# d = Label(window ,text = "Contact Number").grid(row = 3,column = 0)
-# a1 = Entry(window, textvariable= fist_var).grid(row = 0,column = 1)
-# b1 = Entry(window, textvariable= last_var).grid(row = 1,column = 1)
-# c1 = Entry(window, textvariable= email_var).grid(row = 2,column = 1)
-# d1 = Entry(window, textvariable= contact_var).grid(row = 3,column = 1)
+def submit():
+    first = fist_var.get()
+    last = last_var.get()
+    email = email_var.get()
+    contact = contact_var.get()
 
+    item_2 = {
+      "_id" : "U1IT00022",
+      "Name" : first + " " + last,
+      "email" : email,
+      "Contact" : contact,
+   }
+    collection_name.insert_one(item_2)
 
-# def test():
-#     first = fist_var.get()
-#     last = last_var.get()
-#     email = email_var.get()
-#     contact = contact_var.get()
-#     print(first, last, email, contact)
-
-# button = Button(window, text="Hello", command=(test) )
-# button.grid()
-# window.mainloop()
+button = Button(window, text="Hello", command=(submit))
+button.grid(row=4, column=0)
+window.mainloop()
---
+++ pymongo_get_database.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+
+def configure():
+    print('test')
+
+
+def get_database():
+   load_dotenv()
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = f"mongodb+srv://{os.getenv('USERNAME')}:{os.getenv('API_SECRET')}@cluster0.f4werh5.mongodb.net/?retryWrites=true&w=majority"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['GUI_App']
+  
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+   # Get the database
+   dbname = get_database()
